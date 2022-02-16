e5ed8220cc9d0413d8e2ff6c8dc5554dcad714ec
Myunghee13 <myungheel0103@gmail.com>
2022-02-15 22:20:23 -0800

    make sign-in form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
 import React from 'react';
-import HomePage from './pages/homepage/homepage.component';
 import {Switch, Route} from 'react-router-dom';
 
+import HomePage from './pages/homepage/homepage.component';
 import Header from './components/header/header.component';
-
 import ShopPage from './pages/shop/shop.component';
-
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -14,7 +13,8 @@ function App() {
       <Header/>
       <Switch>
         <Route exact path='/' component ={HomePage}/>
-        <Route exact path='/shop' component ={ShopPage}/>
+        <Route path='/shop' component ={ShopPage}/>
+        <Route path='/signin' component = {SignInAndSignUpPage}/>
       </Switch>
     </div>
   );
